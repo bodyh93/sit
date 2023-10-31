8b259e5df2033cef3b9325f09fa76cd51ec88c25
gvadrian <bodyh93@ukr.net>
2023-10-31 21:13:37 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+    <title>Sit</title>
+</head>
+<body>
+<p>Complete form:</p>
+<form action="form.php" method="POST">
+    <input type="text" name="name" placeholder="Your name">
+    <input type="text" name="surname" placeholder="Your surname">
+    <input type="date" name="birthDate" placeholder="Your birthday">
+    <input type="submit" name="submit">
+</form>
+</body>
+</html>
